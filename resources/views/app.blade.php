ffec88f1125f7dbe745504f377d16b547db497f3
RLASH18 <lacdangryan18@gmail.com>
2026-06-15 14:55:53 +0800

    feat(icons): update favicon to use logo.ico

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,8 +30,8 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="icon" href="/logo.ico" sizes="any">
+        {{-- <link rel="icon" href="/favicon.svg" type="image/svg+xml"> --}}
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         <meta name="description" content="Annapolis Security Agency, Inc. (ASAI) — transforming security for a better business." />
